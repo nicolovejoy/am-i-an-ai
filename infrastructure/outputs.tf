61b7a41c9af53f0891e84f7f173cf406e3940a9e
Nico <nlovejoy@ondema.io>
2025-03-05 08:19:45 -0800

    missed a couple

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,7 +1,19 @@
-output "cloudfront_distribution_domain" {
-  value = aws_cloudfront_distribution.website.domain_name
+output "website_url" {
+  description = "Website URL"
+  value       = "https://${var.domain_name}"
 }
 
-output "website_bucket" {
-  value = aws_s3_bucket.website.bucket
+output "cloudfront_distribution_id" {
+  description = "CloudFront distribution ID"
+  value       = aws_cloudfront_distribution.website.id
+}
+
+output "s3_bucket_name" {
+  description = "Name of the S3 bucket hosting the website"
+  value       = aws_s3_bucket.website.id
+}
+
+output "certificate_arn" {
+  description = "ARN of the SSL certificate"
+  value       = aws_acm_certificate.website.arn
 } 
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -2,4 +2,26 @@ variable "aws_region" {
   description = "AWS region for most resources"
   type        = string
   default     = "us-west-2"
+}
+
+variable "domain_name" {
+  description = "The domain name for the website"
+  type        = string
+  default     = "amianai.com"
+}
+
+variable "environment" {
+  description = "Environment (dev/staging/prod)"
+  type        = string
+  default     = "prod"
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+  default = {
+    Project     = "amianai"
+    Environment = "prod"
+    Terraform   = "true"
+  }
 } 
