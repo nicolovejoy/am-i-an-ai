aab327413912b4e10cf72b896684afce39bccd37
Nico <nlovejoy@ondema.io>
2025-03-06 11:47:18 -0800

    work on OIDC config for github workflow

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -148,8 +148,9 @@ resource "aws_iam_openid_connect_provider" "github" {
   
   client_id_list = ["sts.amazonaws.com"]
   
+  # Updated thumbprint list from GitHub docs
   thumbprint_list = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1"
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
   ]
 
   tags = var.tags
@@ -165,15 +166,15 @@ resource "aws_iam_role" "github_actions" {
       {
         Effect = "Allow"
         Principal = {
-          Federated = "token.actions.githubusercontent.com"
+          Federated = aws_iam_openid_connect_provider.github.arn
         }
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
           StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            "${aws_iam_openid_connect_provider.github.url}:aud": "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_username}/amianai:*"
+            "${aws_iam_openid_connect_provider.github.url}:sub": "repo:nicolovejoy/am-i-an-ai:*"
           }
         }
       }
