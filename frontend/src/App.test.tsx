e48eef63f49c28a44c1d95b561594b2fb256cd1c
Nico <nlovejoy@ondema.io>
2025-03-12 19:57:26 -0700

    remove create react app front end

--- frontend/src/App.test.tsx
+++
@@ -1,6 +0,0 @@
-import '@testing-library/jest-dom';
-
-// Skip testing App component for now due to router dependency issues
-test('App component test skipped', () => {
-  expect(true).toBe(true);
-});
